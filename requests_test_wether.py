dfba9f1a14bd5d8733c607ec4d5ceb05e6dedde0
MIxanic <itmixanic@gmail.com>
2024-11-19 10:36:52 +0300

    Refactoring кода

--- requests_test_wether.py
+++ requests_test_wether.py
@@ -1,48 +1,32 @@
 import requests
 import pandas as pd
-wth_full_info = []
+from config import KeyWether
 
-def get_wether(city, date):
+key = KeyWether
 
-    wth_info = {
-        'location': None,
-        'date': None,
-        'min_temp': None,
-        'max_temp': None,
-        'avg_temp': None,
-    }
-
-    URL = f'http://api.weatherapi.com/v1/future.json?key=ddaed6ca34ee4664b86203047240411&q={city}&dt={date}'
+def get_wether(city, days):
+    wth_full_info = []
+    URL = f'http://api.weatherapi.com/v1/forecast.json?key={key}&q={city}&days={days}&aqi=no&alerts=yes'
     r = requests.get(url=URL)
 
     resul = r.json()
 
-    wth_info['location'] = resul.get('location').get('name')
-    wth_info['date'] = resul.get('forecast').get('forecastday')[0].get('date')
-    wth_info['max_temp'] = resul.get('forecast').get('forecastday')[0].get('day').get('maxtemp_c')
-    wth_info['min_temp'] = resul.get('forecast').get('forecastday')[0].get('day').get('mintemp_c')
-    wth_info['avg_temp'] = resul.get('forecast').get('forecastday')[0].get('day').get('avgtemp_c')
+    for i in range(days):
+        wth_info = {
+            'location': resul.get('location').get('name'),
+            'date': resul.get('forecast').get('forecastday')[i].get('date'),
+            'min_temp': resul.get('forecast').get('forecastday')[i].get('day').get('maxtemp_c'),
+            'max_temp': resul.get('forecast').get('forecastday')[i].get('day').get('mintemp_c'),
+            'avg_temp': resul.get('forecast').get('forecastday')[i].get('day').get('avgtemp_c'),
+        }
 
-    wth_full_info.append(wth_info)
-    return wth_info
+        wth_full_info.append(wth_info)
 
-    # Если бы использовали библиотеку glom
-    # spec = {
-    #     'location': ('location:name'),
-    #     'date': ('forecast:forecastday:0:date'),
-    #     'max_temp': ('forecast:forecastday:0:day:maxtemp_c'),
-    #     'min_temp': ('forecast:forecastday:0:day:mintemp_c'),
-    #     'avg_temp': ('forecast:forecastday:0:day:avgtemp_c'),
-    # }
-    #
-    # wth_info = glom.glom(resul, spec, default=None)
+    return wth_full_info
 
-get_wether('Moscow', '2024-12-05')
-get_wether('Moscow', '2024-12-06')
-get_wether('Moscow', '2024-12-07')
-get_wether('Moscow', '2024-12-08')
+wth = get_wether('Moscow', 3)
 
-df1 = pd.DataFrame(wth_full_info)
+df1 = pd.DataFrame(wth)
 df1.to_csv("wether_info.csv", index=False)
 
 get_df1 = pd.read_csv("wether_info.csv")
